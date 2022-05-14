e5e0a1d5e85f33db5ad17943c60714ec8c08eb93
Soma Ikeda <soma@Soma-MacBook-Air.local>
2022-05-14 15:42:26 +0900

    added package.json

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,2 +1,8 @@
-import { createStore, applyMiddleware } from 'redux';
+import { legacy_createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+
+const initialState = {};
+
+export const initializeState = (preloadedState = initialState) => {
+    const middleware = [];
+};
