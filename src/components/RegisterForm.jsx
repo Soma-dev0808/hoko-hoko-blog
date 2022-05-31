ea70bedc37b53fcc6c46b3ec1b15126da2646f04
michiru <MICHIRU@michiru-2.local>
2022-05-31 21:43:11 +0900

    border,box size change

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -8,17 +8,17 @@ import './Register.css';
 const RegisterForm = () => {
     return (
         <section className="register">
-            <h1 className="title"> Register</h1>
-            <div className="説明"> Create your account. Its free and only takes a minute</div>
-            <input className="firstname inputs" placeholder="First Name" />
-            <input className="lastname inputs" placeholder="Last Name" />
-            <input className="email inputs" placeholder="Email" />
-            <input className="password inputs" placeholder="Password" />
-            <input className="confirmpassword inputs" placeholder="Confirm Password" />
+            <h2 className="title"> Register</h2>
+            <div className="explanation"> Create your account. Its free and only takes a minute</div>
+            <input className="firstname inputs name" placeholder="First Name" />
+            <input className="lastname inputs name" placeholder="Last Name" />
+            <input className="email input2" placeholder="Email" type="email" />
+            <input className="password input2" placeholder="Password" />
+            <input className="confirmpassword input2" placeholder="Confirm Password" />
             <input type="checkbox" value="agreed" />
             <span>I accept the </span>
             <a href="https://www.lovebakesgoodcakes.com/canadian-meat-pie/" target="_blank" rel="noreferrer">Terms of Use</a>
-            and
+            <span> & </span>
             <a href="https://upload.wikimedia.org/wikipedia/commons/8/82/Oia%2C_Santorini_HDR_sunset.jpg" target="_blank" rel="noreferrer">Privacy Policy</a>
             <input className="submit" type="submit" value="Register Now" />
 
